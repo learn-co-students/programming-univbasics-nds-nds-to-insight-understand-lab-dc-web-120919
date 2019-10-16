679ac7b5ded7fe5839da9d190e46896efdc25b61
J <jmrumingan@gmail.com>
2019-10-16 15:58:26 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,31 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp nds 
 end
 
-def print_first_directors_movie_titles
-end
+def print_first_directors_movie_titles(nds)
+  list_of_movies = []
+  array_of_keys = [] #we don't know what the encrypted keys are 
+  nds.each_key do |key|
+    array_of_keys << key 
+  end 
+  
+  first_counter = 0 
+  while first_counter < nds.length do 
+    #puts "First layer #{first_counter} has #{nds[first_counter]} hashes"
+    second_counter = 0 
+    array_of_keys2 = []
+    nds[array_of_keys[first_counter]].each_key do |key|
+      array_of_keys2 << key 
+    end 
+    
+    while second_counter<nds[array_of_keys][array_of_keys2].length do 
+      list_of_movies<<nds[array_of_keys][array_of_keys2][second_counter]
+    end 
+    
+  end 
+  puts list_of_movies
+  return list_of_movies
+end 
+
